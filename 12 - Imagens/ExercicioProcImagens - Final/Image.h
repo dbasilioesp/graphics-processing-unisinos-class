f6853581ade14ef2addf6c49fe5d446e91d9e703
David Basilio <david.basilio@azion.com>
2014-05-27 09:34:17 -0300

    Created folder with code in process to Image class

---
+++ 12 - Imagens/ExercicioProcImagens - Final/Image.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class Image {
+public:
+	Image(int width, int height){
+		this->width = width;
+		this->height = height;
+		pixels = new int[width*height];
+	}
+	~Image(void){
+		delete[] pixels;
+	}
+
+	void setRGB(int x, int y, int rgb){
+		pixels[x + y * width] = rgb;
+	}
+
+	int getRGB(int x, int y){
+		return pixels[x + y * width];
+	}
+
+	int getWidth(){ return width; }
+	int getHeight(){ return height; }
+
+	int* getPixels(){ return pixels; }
+
+private:
+	int width, height;
+	int *pixels;
+};
